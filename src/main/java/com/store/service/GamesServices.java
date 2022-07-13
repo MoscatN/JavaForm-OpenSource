f82cc278329db93fc2ab23bc4d7b21bbb303463c
MoscatN <86422286+MoscatN@users.noreply.github.com>
2022-07-13 13:48:50 -0400

    Update GamesServices.java

--- src/main/java/com/store/service/GamesServices.java
+++ src/main/java/com/store/service/GamesServices.java
@@ -2,7 +2,11 @@ package com.store.service;
 
 import java.util.List;
 import com.store.domain.Games;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+
+@Service
 public interface GamesServices {
     public List<Games> GamesList();
     
